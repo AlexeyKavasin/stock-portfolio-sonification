8a4716a6b7d9d25f34c528ab0d8e5f4acebc8d61
akavasin <akavasin@gmail.com>
2021-11-14 18:46:43 +0300

    tone wip, sheets api web, etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,74 @@
-import type { NextPage } from 'next';
-import StockHelper from '../lib/StockHelper';
-import { composeStockData } from '../utils/composeStockData';
-import styles from '../styles/Main.module.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import * as Tone from 'tone';
+import React, { useState, useEffect } from 'react';
+import useSWR from 'swr';
+import { credentials } from '../credentials';
+import { composeStockData, IStockData } from '../utils/composeStockData';
+import { SequencePattern } from '../lib/tone-helpers';
+import styles from '../styles/Main.module.css'; 
 
-const stockHelper = new StockHelper();
+const fetcher = (...args: [any]) => fetch(...args).then(res => res.json())
 
-const Index: NextPage = ({ data }: any) => {
-  const rows = data && data.length ? data[0].rowData : [];
-  const stockData = rows.length ? composeStockData(rows.slice(1, rows.length - 1)) : [];
+function Index() {
+  // TODO
+  // data doesn't change on saturdays and sundays, no need to fetch
+  const { data } = useSWR(credentials.web_api, fetcher, { refreshInterval: 30000 });
+  const [soundOn, turnSoundOn] = useState<boolean>(false);
+  const [stockData, setStockData] = useState<IStockData[]>([]);
+
+  let interval: NodeJS.Timer | null = null;
+  let pattern1: any = null;
+
+  const createAudio = () => {
+    if (!pattern1) {
+      pattern1 = new SequencePattern();
+    } else {
+      pattern1.destroy();
+      pattern1.build(null, null, null, '8n');
+    }
+  };
+
+  const startTimer = () => {
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+      // update with fetched data
+      setStockData(composeStockData(data));
+      createAudio();
+    }, 60000)
+  }
+
+  const toggleSound = () => {
+    turnSoundOn(!soundOn);
+
+    if (!soundOn) {
+      // play sound
+      if (Tone.context.state !== 'running') {
+        Tone.context.resume();
+      }
+
+      Tone.Transport.start();
+    } else {
+      Tone.Transport.pause();
+    }
+  };
+
+  useEffect(() => {
+    if (data && !stockData.length) {
+      // and data !== prev.data
+      setStockData(composeStockData(data));
+      createAudio();
+      startTimer();
+    }
+
+  }, [data, soundOn, stockData])
 
   return (
     <div>
       <main className={styles.main}>
-        💰
+        <button onClick={toggleSound}>{soundOn ? 'Sound off' : 'Sound on'}</button>
       </main>
       {stockData && stockData.length ? (
         <footer>
@@ -29,17 +85,6 @@ const Index: NextPage = ({ data }: any) => {
       ) : null}
     </div>
   )
-}
-
-export async function getStaticProps() {
-  await stockHelper.initializeApiClient();
-
-  return {
-    props: {
-      data: await stockHelper.fetchStockData(),
-    },
-    revalidate: 10,
-  };
 }
 
 export default Index;
--- utils/composeStockData.ts
+++ utils/composeStockData.ts
@@ -10,25 +10,8 @@ export interface IStockData {
   ticker: string;
   changePct: number;
   share: number;
-  price: number;
 }
 
-export function composeStockData(rows: any): IStockData[] {
-  if (!rows || !rows.length) {
-    return [];
-  }
-
-  return rows.reduce((acc: IGoogleRow[], item: IGoogleRow) => {
-    const ticker = item.values[0].formattedValue;
-    const changePct = Number(item.values[3].formattedValue?.replace(',', '.'));
-    const price = Number(item.values[5].formattedValue?.replace(',', '.'));
-    const share = Number(item.values[7].formattedValue?.replace(',', '.'));
-    
-    return [...acc, {
-      ticker,
-      changePct,
-      share,
-      price,
-    }];
-  }, []);
+export function composeStockData(data: any): IStockData[] {
+  return data && data.stocks ? data.stocks : [];
 }
